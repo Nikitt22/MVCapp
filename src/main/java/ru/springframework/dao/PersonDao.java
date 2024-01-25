ebb0cda2e166969c6baa668d72e36c34f8737ba6
Nikitt22 <87071043+Nikitt22@users.noreply.github.com>
2024-01-25 15:26:23 +0800

    Приложение только читает список и выводит в браузер.

---
+++ src/main/java/ru/springframework/dao/PersonDao.java
@@ -0,0 +1,25 @@
+package ru.springframework.dao;
+
+import org.springframework.stereotype.Component;
+import ru.springframework.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+@Component
+public class PersonDao {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+    {
+        people = new ArrayList<>();
+        people.add( new Person(PEOPLE_COUNT++, "Tom"));
+        people.add( new Person(PEOPLE_COUNT++, "Jerry"));
+        people.add( new Person(PEOPLE_COUNT++, "John"));
+        people.add( new Person(PEOPLE_COUNT++, "Katy"));
+    }
+    public List<Person> index(){
+        return people;
+    }
+    public Person show(int id){
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}

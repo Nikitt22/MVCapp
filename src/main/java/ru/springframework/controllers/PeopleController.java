5f69b6cedf423ebecb4940834ede414c63eb5dfe
Nikitt22 <87071043+Nikitt22@users.noreply.github.com>
2024-02-03 08:47:16 +0800

    Приложение умеет записывать в список и выводить

--- src/main/java/ru/springframework/controllers/PeopleController.java
+++ src/main/java/ru/springframework/controllers/PeopleController.java
@@ -4,29 +4,41 @@ package ru.springframework.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.springframework.dao.PersonDao;
+import ru.springframework.models.Person;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
-    private PersonDao personDao;
+    private final PersonDao personDao;
+
     @Autowired
-    public PeopleController(PersonDao personDao) {
-        this.personDao = personDao;
+    public PeopleController(PersonDao PersonDao) {
+        this.personDao = PersonDao;
     }
 
     @GetMapping()
-    public String index(Model model){
-    model.addAttribute("people", personDao.index());
+    public String index(Model model) {
+        model.addAttribute("people", personDao.index());
         return "people/index";
     }
+
     @GetMapping("/{id}")
-    public String show(@PathVariable("id") int id, Model model){
-    model.addAttribute("person", personDao.show(id));
-        return  "people/show";
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDao.show(id));
+        return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDao.save(person);
+        return "redirect:/people";
     }
 }
--- src/main/java/ru/springframework/dao/PersonDao.java
+++ src/main/java/ru/springframework/dao/PersonDao.java
@@ -11,15 +11,21 @@ public class PersonDao {
     private List<Person> people;
     {
         people = new ArrayList<>();
-        people.add( new Person(PEOPLE_COUNT++, "Tom"));
-        people.add( new Person(PEOPLE_COUNT++, "Jerry"));
-        people.add( new Person(PEOPLE_COUNT++, "John"));
-        people.add( new Person(PEOPLE_COUNT++, "Katy"));
+        people.add( new Person(++PEOPLE_COUNT, "Tom"));
+        people.add( new Person(++PEOPLE_COUNT, "Jerry"));
+        people.add( new Person(++PEOPLE_COUNT, "John"));
+        people.add( new Person(++PEOPLE_COUNT, "Katy"));
     }
     public List<Person> index(){
         return people;
     }
     public Person show(int id){
         return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+        System.out.println(people);
     }
 }
